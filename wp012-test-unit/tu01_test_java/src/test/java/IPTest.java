96bbe236c95a67392968f42af5562bab690375dd
ganyankai <784979813@qq.com>
2020-07-27 17:31:42 +0800

    2020年7月27日

---
+++ wp012-test-unit/tu01_test_java/src/test/java/IPTest.java
@@ -0,0 +1,20 @@
+import java.net.*;
+
+public class IPTest {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        InetAddress ia = null;
+        try {
+            ia = ia.getLocalHost();
+            String localname = ia.getHostName();
+            String localip = ia.getHostAddress();
+            System.out.println("本机名称是：" + localname);
+            System.out.println("本机的ip是 ：" + localip);
+        } catch (Exception e) {
+        // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+}
